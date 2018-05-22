f17cca65755d703b17bb023d81bfa685ec7e855f
LarisaGl <89888985265@mail.ru>
2018-05-22 17:28:38 +0300

    +list.php

--- admin.php
+++ admin.php
@@ -13,11 +13,19 @@
 
 <?php
 if (!empty ($_FILES) && array_key_exists('ourfile', $_FILES)) {
-	move_uploaded_file($_FILES['ourfile']['tmp_name'], 't1.php');
+	move_uploaded_file($_FILES['ourfile']['tmp_name'], 't1.json');
 
-	$json = file_get_contents('t1.php');
-	$decode = json_decode($json, true);
+	$json = file_get_contents('t1.json');
+	$decode = json_decode($json, true);	
+
+	$list = fopen("list.php", "a");
+	$data = $_FILES['ourfile']['name'];
+	$test = fwrite($list, $data);
+	if ($test) echo 'Данные в файл успешно занесены. Имя теста:'.$_FILES['ourfile']['name'];
+	else echo 'Ошибка при записи в файл.';
+	fclose($list);
 }
+
 ?>
 
 </body>

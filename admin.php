2a49698aa1f2548d914a18e725a1da389551bd7b
LarisaGl <89888985265@mail.ru>
2018-05-22 14:11:05 +0300

    new

--- admin.php
+++ admin.php
@@ -7,34 +7,18 @@
 
 <body>
 <form action="admin.php" method="POST" enctype="multipart/form-data">
-  <input type="file" name="ourfile">  
+  <input type="file" name="ourfile" accept="application/json">  
   <input type="submit" value="Отправить">  
 </form>
 
 <?php
 if (!empty ($_FILES) && array_key_exists('ourfile', $_FILES)) {
-	move_uploaded_file($_FILES['ourfile']['tmp_name'], 'list.php');
+	move_uploaded_file($_FILES['ourfile']['tmp_name'], 't1.php');
 
-	$json = file_get_contents('list.php');
+	$json = file_get_contents('t1.php');
 	$decode = json_decode($json, true);
 }
 ?>
-	
-<?php foreach ($decode as $key => $value) { ?>
-	<form action="test.php" method="GET">	
-	<fieldset>
-    <legend><?php echo $key; ?></legend>
-    	<?php foreach ($value as $k => $v) {
-    		foreach ($v as $e => $val) { ?>
-   		<label><input type="radio" name="name"><?php echo $val?></label>
-    		<?php } ?>
-    	<?php } ?>
-    </fieldset>	
-<input type="submit" value="Отправить">  
-</form>
-<?php } ?>
-
-<?php echo $_GET ['name']; ?>
 
 </body>
 </html>

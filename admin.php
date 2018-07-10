fda475dab11664abea6558dab51186af6fa3dd08
LarisaGl <89888985265@mail.ru>
2018-07-11 00:59:09 +0300

    new

--- admin.php
+++ admin.php
@@ -8,22 +8,19 @@
 <body>
 <form action="admin.php" method="POST" enctype="multipart/form-data">
   <input type="file" name="ourfile" accept="application/json">  
-  <input type="submit" value="Отправить">  
+  <input type="submit" value="Загрузить тест">  
 </form>
 
 <?php
+
+$dir = 'tests/';
+$files = scandir($dir);
+$counts=count($files);
+$num=$counts-1;
+$newFilename="tests/$num.json";
 if (!empty ($_FILES) && array_key_exists('ourfile', $_FILES)) {
-	move_uploaded_file($_FILES['ourfile']['tmp_name'], 't1.json');
-
-	$json = file_get_contents('t1.json');
-	$decode = json_decode($json, true);	
-
-	$list = fopen("list.php", "a");
-	$data = $_FILES['ourfile']['name'];
-	$test = fwrite($list, $data);
-	if ($test) echo 'Данные в файл успешно занесены. Выберите тест:'.'<br>'.'<a href="test.php">'.$_FILES['ourfile']['name'].'</a>';
-	else echo 'Ошибка при записи в файл.';
-	fclose($list);
+	move_uploaded_file($_FILES['ourfile']['tmp_name'], $newFilename);
+	echo "Файл сохранен с названием: $num.json";
 }
 
 ?>

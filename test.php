bafbe3029504843c175575ddda5a5619b8d4d861
LarisaGl <89888985265@mail.ru>
2018-07-12 20:14:23 +0300

    last version

--- test.php
+++ test.php
@@ -11,34 +11,34 @@
 $filename=$_GET['test'];
 $json = file_get_contents("tests/$filename");
 $decode = json_decode($json, true);
-$i=1;
 ?>
 
 
-<form action="test.php" method="POST">	
-	<?php
-	foreach ($decode as $num) {
-		foreach ($num as $quest) {
-			$i=$i+1;?>
-	<fieldset>
-	<legend><?php echo $quest['ask']; ?></legend>
-	<?php
-	foreach ($quest as $var) {
-		if (is_array($var)) {
-			foreach ($var as $var_ans) {
-				foreach ($var_ans as $ans) { ?>
-    <label><input type="radio" name="name_<?php echo $i?>"><?php echo $ans;?></label>
-     <?php
- 	}
- 		}
-			}
-    			} ?>
-    </fieldset>
+<form action="" method="POST">	
     <?php
-	}
-		} ?>
-	<input type="submit" value="Отправить">  
+        foreach ($decode as $num => $quest) { ?>
+	<fieldset>
+        <legend><?php echo $quest['ask']; ?></legend>
+        <?php
+            foreach ($quest['answer'] as $var_ans => $ans) {?>
+        <label><input type="radio" name="<?php echo $num?>" value="<?php echo $var_ans;?>"><?php echo $ans; ?></label>
+    <?php } ?>
+    </fieldset>
+    <?php } ?>
+	<input type="submit" value="Узнать результат" name="result">  
 </form>
+
+<?php
+if (isset($_POST['result'])) {
+	$i=0;
+	foreach ($decode as $key => $value) {
+		if ($value['right_answer']==$_POST[$key]) {
+		$i++;
+		}
+	}
+	echo "Правильных ответов: $i";
+}
+?>
 
 </body>
 </html>
